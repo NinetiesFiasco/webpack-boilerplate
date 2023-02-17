f4899cbd653f471845134762602d46732d3139ed
Seredin Anton <seredin.anton2014@gmail.com>
2023-02-17 18:40:54 +0530

    setup environment  with webpack

---
+++ webpack.config.client.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+const CURRENT_WORKING_DIR = process.cwd()
+const nodeExternals = require('webpack-node-externals')
+const { VueLoaderPlugin } = require('vue-loader')
+
+
+const config = {
+  name: "browser",
+  mode: "development",
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(CURRENT_WORKING_DIR, 'client/main.js')
+  ],
+  output: {
+    path: path.join(CURRENT_WORKING_DIR, '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        use: ['vue-loader']
+      },{
+        test: /\.js$/,
+        use: ['babel-loader']
+      },{
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      },{
+        test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new VueLoaderPlugin()
+  ]
+}
+
+module.exports = config
---
+++ webpack.config.client.production.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const webpack = require('webpack')
+const CURRENT_WORKING_DIR = process.cwd()
+const nodeExternals = require('webpack-node-externals')
+const { VueLoaderPlugin } = require('vue-loader')
+
+
+const config = {
+  mode: "production",
+  entry: [
+    path.join(CURRENT_WORKING_DIR, 'client/main.js')
+  ],
+  output: {
+    path: path.join(CURRENT_WORKING_DIR, '/dist'),
+    filename: 'bundle.js',
+    publicPath: "/dist/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        use: ['vue-loader']
+      },{
+        test: /\.js$/,
+        use: ['babel-loader']
+      },{
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      },{
+        test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+
+}
+
+module.exports = config
